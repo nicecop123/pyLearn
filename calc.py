103921e4ebca61540f7956dec9518a75243ab822
nicecop123 <utkrisht.thaman@gmail.com>
2020-04-16 22:25:14 +0100

    4th try, some improvement
    Signed-off-by: nicecop123 <utkrisht.thaman@gmail.com>

--- calc.py
+++ calc.py
@@ -54,6 +54,14 @@ def button_divide():
     f_num = int(first_number)
     numberentry.delete(0, END)
 
+def button_square():
+    first_number = numberentry.get()
+    global f_num
+    global math
+    math = "square"
+    f_num = int(first_number)
+    numberentry.delete(0, END)
+
 def button_equal():
     second_number = numberentry.get()
     numberentry.delete(0,END)
@@ -65,6 +73,8 @@ def button_equal():
         numberentry.insert(0,f_num*int(second_number))
     if math == "division":
         numberentry.insert(0,f_num/int(second_number))
+    if math == "square":
+        numberentry.insert(0,f_num*f_num)
 
 button9 = Button(topframe, text="9", fg="orange",command=lambda: button_click(9))
 button8 = Button(topframe, text="8", fg="orange",command=lambda: button_click(8))
@@ -82,24 +92,26 @@ buttonmultiply =Button(bottomframe, text="x", fg="orange",command=button_multipl
 buttondivide = Button(morebottom, text="/", fg="orange",command=button_divide)
 buttonclear = Button(morebottom, text="Clear", fg="orange",command=button_clear)
 buttonequal = Button(morebottom, text="=", fg="orange",command=button_equal)
+buttonsquare =  Button(morebottom, text="ˆ2", fg="orange",command=button_square)
 
-numberentry.pack()
+numberentry.grid(row=0, column=0)
 
-button9.pack(side=LEFT)
-button8.pack(side=LEFT)
-button7.pack(side=LEFT)
-buttonadd.pack(side=LEFT)
-button6.pack(side=LEFT)
-button5.pack(side=LEFT)
-button4.pack(side=LEFT)
-buttonminus.pack(side=LEFT)
-button3.pack(side=LEFT)
-button2.pack(side=LEFT)
-button1.pack(side=LEFT)
-buttonmultiply.pack(side=LEFT)
-buttonclear.pack(side=LEFT)
-button0.pack(side=LEFT)
-buttonequal.pack(side=LEFT)
-buttondivide.pack(side=LEFT)
+button9.grid(column=0,row=1)
+button8.grid(column=1,row=1)
+button7.grid(column=2,row=1)
+buttonadd.grid(column=3,row=1)
+button6.grid(column=0,row=2)
+button5.grid(column=1,row=2)
+button4.grid(column=2,row=2)
+buttonminus.grid(column=3,row=2)
+button3.grid(column=0,row=3)
+button2.grid(column=1,row=3)
+button1.grid(column=2,row=3)
+buttonmultiply.grid(column=3,row=3)
+buttonclear.grid(column=3,columnspan=2,row=4)
+button0.grid(column=1,row=4)
+buttonequal.grid(column=0,row=5)
+buttondivide.grid(column=0,row=4)
+buttonsquare.grid(column=1,row=5)
 
 root.mainloop()
